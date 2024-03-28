24b8ba4f8a6961995e95f80f4224cef6ff363426
Harini Allu <120156889+alluharini@users.noreply.github.com>
2024-03-28 09:17:24 +0530

    Update day-2  (03-GNB algorithm display by confusion matrix).py

--- day-2  (03-GNB algorithm display by confusion matrix).py
+++ day-2  (03-GNB algorithm display by confusion matrix).py
@@ -6,13 +6,14 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 iris = load_iris()
 X = iris.data
 y = iris.target
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
+
 conf_matrix = confusion_matrix(y_test, y_pred)
 print("Confusion Matrix:")
 print(conf_matrix)
